c6129bcc6ea820643685f2db014d885e626ba0da
unknown <yusef.abdullah96@gmail.com>
2024-11-14 14:53:58 +0600

    radial chart

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -10,46 +10,19 @@ import {
 
 const data = [
   {
-    name: "18-24",
-    uv: 31.47,
-    pv: 2400,
-    fill: "#8884d8",
+    name: "Total",
+    count: 106,
+    fill: "white",
+  },
+  {
+    name: "Girls",
+    count: 50,
+    fill: "#fae27c",
   },
   {
     name: "25-29",
-    uv: 26.69,
-    pv: 4567,
-    fill: "#83a6ed",
-  },
-  {
-    name: "30-34",
-    uv: 15.69,
-    pv: 1398,
-    fill: "#8dd1e1",
-  },
-  {
-    name: "35-39",
-    uv: 8.22,
-    pv: 9800,
-    fill: "#82ca9d",
-  },
-  {
-    name: "40-49",
-    uv: 8.63,
-    pv: 3908,
-    fill: "#a4de6c",
-  },
-  {
-    name: "50+",
-    uv: 2.63,
-    pv: 4800,
-    fill: "#d0ed57",
-  },
-  {
-    name: "unknow",
-    uv: 6.67,
-    pv: 4800,
-    fill: "#ffc658",
+    count: 50,
+    fill: "#c3ebfa",
   },
 ];
 
@@ -69,28 +42,23 @@ const CountChart = () => {
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
       {/*Chart*/}
-      <div className="w-full h-[75%]">
+      <div className="relative w-full h-[75%]">
         <ResponsiveContainer>
           <RadialBarChart
             cx="50%"
             cy="50%"
-            innerRadius="10%"
-            outerRadius="80%"
-            barSize={10}
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
             data={data}
           >
             <RadialBar
-              label={{ position: "insideStart", fill: "#fff" }}
               background
-              dataKey="uv"
-            />
-            <Legend
-              iconSize={10}
-              layout="vertical"
-              verticalAlign="middle"
+              dataKey="count"
             />
           </RadialBarChart>
         </ResponsiveContainer>
+        <Image src="/maleFemale.png" alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
       </div>
       {/*Bottom */}
       <div className="flex justify-center gap-16">
